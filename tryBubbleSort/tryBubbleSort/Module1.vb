f859d874f3156e919fdf93350ff05af613e1b198
HasanMahdi <49053509+SyedHasanMahdi@users.noreply.github.com>
2025-11-04 22:56:59 +0300

    Add files via upload

---
+++ tryBubbleSort/tryBubbleSort/Module1.vb
@@ -0,0 +1,41 @@
+﻿Imports System.CodeDom.Compiler
+
+Module Module1
+    '   Title:          tryBubbleSort
+    '   Author:         Hasan
+    '   Version:        0.1
+    '   Date:           4/11/2025
+    '   Desc:           Trying out bubble sort algorithm
+
+    Sub Main()
+        Dim myArr(9) As Integer
+        myArr(0) = 17
+        myArr(1) = 84
+        myArr(2) = 3
+        myArr(3) = 5
+        myArr(4) = 87
+        myArr(5) = 110
+        myArr(6) = 24
+        myArr(7) = 36
+        myArr(8) = 208
+        myArr(9) = 19
+        myArr(10) = 1
+
+        Dim swap As Boolean = True
+        Dim temp As Integer
+
+        While swap = True
+            swap = False
+            For x = 0 To 9
+                If myArr(x) > myArr(x + 1) Then
+                    '   Swap
+                    temp = myArr(x)
+                    myArr(x) = myArr(x + 1)
+                    myArr(x + 1) = temp
+                    swap = True
+                End If
+            Next x
+        End While
+    End Sub
+
+End Module
---
+++ tryBubbleSort/tryBubbleSort/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("tryBubbleSort")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("tryBubbleSort")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ecd2cb31-68b6-4fd0-b087-2c7a21244b6e")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
