c2d530264828089ab7b14adf799212bf21882730
HasanMahdi <49053509+SyedHasanMahdi@users.noreply.github.com>
2025-11-04 23:19:34 +0300

    Add files via upload

---
+++ tryFuel/tryFuel/Module1.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+    '   Title:          Tryfuel
+    '   Author:         
+    '   Version:        0.1
+    '   Date:           10/09/2023
+    '   Description:    Calculate fuel stuff for a car
+    Sub Main()
+        Dim CostPerLitre As Double = 2.1
+        Dim TotalFuel As Integer = 70
+        Dim KmPerFillup As Integer = 352
+        Dim CostPerKM As Double = 0
+        Dim KmPerLitre As Double = 0
+        Dim MilesPerGallon As Double = 0
+        Dim HundredKmLitre As Double = 0
+        '   Declaring Variables
+
+        CostPerKM = Math.Round((KmPerFillup / (CostPerLitre * TotalFuel)), 2)
+        KmPerLitre = Math.Round((KmPerFillup / TotalFuel), 2)
+        MilesPerGallon = Math.Round((KmPerLitre * 2.352), 2)
+        HundredKmLitre = Math.Round((KmPerLitre * 100), 2)
+
+        Console.WriteLine(" The car costs QAR" & CostPerKM & " per kilometre and drives " & KmPerLitre & "km per litre of fuel, which can also be written as " & MilesPerGallon & "miles per gallon" & " and it consumes " & HundredKmLitre & " litres per 100km")
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ tryIteration/tryIteration/Module1.vb
@@ -0,0 +1,34 @@
+﻿Module Module1
+    '   Title:      tryIteration
+    '   Author:     ME
+    '   Version:    0.1
+    '   Date:       08/10/2023
+    '   Description:    Working with iterative statements
+    Sub Main()
+        '   Output the even numbers from 0 to 100
+
+        Dim num1, num2, Steps As Integer
+        Console.WriteLine("Please enter the starting value")
+        num1 = Console.ReadLine
+        Console.WriteLine("please enter the limit")
+        num2 = Console.ReadLine
+        Console.WriteLine("Enter a step for this list")
+        Steps = Console.ReadLine()
+
+        If num1 < num2 Then
+            Console.WriteLine("Num1 is: " & num1)
+            Console.WriteLine("Num2 is: " & num2)
+            For index = num2 To num2 Step Steps
+                Console.WriteLine(index)
+            Next
+        Else
+            For index = num2 To num1 Step (-Steps)
+                Console.WriteLine("Num1 is: " & num1)
+                Console.WriteLine("Num2 is: " & num2)
+                Console.WriteLine(index)
+            Next
+        End If
+        Console.ReadLine()
+    End Sub
+
+End Module
